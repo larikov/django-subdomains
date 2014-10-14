da6c78adc27d60b2ec16a1208e8a4ad020c5aca6
Alexander Larikov <larikov@dubizzle.com>
2014-10-14 13:15:36 +0400

    django 1.7

--- subdomains/__init__.py
+++ subdomains/__init__.py
@@ -1,1 +1,3 @@
-__version__ = (0, 0, 2)
+__version__ = (0, 0, 3)
+
+default_app_config = 'subdomains.apps.SubdomainsConfig'
---
+++ subdomains/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class SubdomainsConfig(AppConfig):
+	name = 'subdomains'
+	verbose_name = 'Django Subdomains'
